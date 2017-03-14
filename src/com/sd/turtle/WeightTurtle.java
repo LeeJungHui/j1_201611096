e77e0bdfef307fe84edd0a15486a4e519d2d61f2
LeeJungHui <sowew54@naver.com>
2017-03-14 14:28:32 +0900

    Add files via upload

---
+++ src/com/sd/turtle/WeightTurtle.java
@@ -0,0 +1,17 @@
+package com.sd.turtle;
+  
+import ch.aplu.turtle.*;
+import java.awt.Color;
+
+
+public class WeightTurtle extends Turtle {
+    protected int weight;
+    public WeightTurtle(int w) {
+        weight=w;
+        setColor(Color.red);
+        setPenColor(Color.red);
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
