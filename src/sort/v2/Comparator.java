64bc562f98e0303466f85e6e909ef8f599a9fbaf
LeeJungHui <sowew54@naver.com>
2017-03-28 01:02:30 +0900

    Delete Comparator.java

--- src/sort/v2/Comparator.java
+++
@@ -1,7 +0,0 @@
-package com.sd.sort.v2;
-import ch.aplu.turtle.*;
-import com.sd.turtle.WeightTurtle;
-interface Comparator {
-    public int compare(Object o1, Object o2);
-    public boolean equals(Object o);
-}
