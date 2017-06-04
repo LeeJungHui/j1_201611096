b35307fb9f147cac016091a09da4d391817d9066
LeeJungHui <sowew54@naver.com>
2017-06-05 01:34:29 +0900

    Add files via upload

--- src/com/sd/dao/v0/DaoMainV0.java
+++ src/com/sd/dao/v0/DaoMainV0.java
@@ -8,6 +8,7 @@ public class DaoMainV0
   {
     Connection conn = null;
     Statement stmt =null;
+    ResultSet rs = null;
     try{
       String path=System.getProperty("user.dir");
       path += "/src/mysql.properties";
@@ -15,15 +16,29 @@ public class DaoMainV0
       prop.load(new FileInputStream(path));
       Class.forName("com.mysql.jdbc.Driver");
       //conn = DriverManager.getConnection("jdbc:mysql://localhost/test","root","rjdnf88asdf");
-      conn=DriverManager.getConnection(prop.getProperty("URL"),prop.getProperty("USER"),prop.getProperty("PASSWORD"));
+      conn=DriverManager.getConnection(prop.getProperty("URL")+"?useSSL=true",prop.getProperty("USER"),prop.getProperty("PASSWORD"));
+      //String mySql="CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
+      //mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
+      //stmt.execute(mySql);
+      /*stmt.execute("INSERT INTO persons(name,address) VALUES('v0-1','1 Hongji Dong')");
+      stmt.execute("INSERT INTO persons(name,address) VALUES('v0-2','2 Hongji Dong')");
+      stmt.execute("INSERT INTO persons(name,address) VALUES('v0-3','3 Hongji Dong')");
+      ////persons(id,name,address) 이렇게 하면 id까지 지정가능
+      stmt.execute("DELETE FROM persons WHERE id=3 LIMIT 1");
+      stmt.execute("select name from persons where id<4 order by id");*/
+      String mySql="select name from persons where id<4 order by id";
       stmt = conn.createStatement();
-      String mySql="CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
-      mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
-      stmt.execute(mySql);
+      rs= stmt.executeQuery(mySql);
+      while(rs.next())
+      {
+        System.out.println(rs.getString("name"));
+      }
+
     } catch(Exception e){
       e.printStackTrace();
     } finally{
       try{
+        if(rs != null) rs.close();
         if(stmt !=null) stmt.close();
         if(conn !=null) conn.close();
       } catch(Exception e){};
